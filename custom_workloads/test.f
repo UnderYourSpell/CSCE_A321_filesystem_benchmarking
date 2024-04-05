8312705c5f915a0ea41b199103e8a86a5a7b7501
Moro Bamber <99629944+UnderYourSpell@users.noreply.github.com>
2024-04-05 10:56:13 -0800

    Update test.f

--- custom_workloads/test.f
+++ custom_workloads/test.f
@@ -1,8 +1,8 @@
 #Test Workload for OS
 
-set $nthreads=2
+set $nthreads=4
 
-define fileset name="testF",entries=100,filesize=1k,prealloc,path="/tmp"
+define fileset name="testF",entries=1000,filesize=1k,prealloc,path="/tmp"
 
   define process name="readerP",instances=1 {
     thread name="readerT",instances=$nthreads {
